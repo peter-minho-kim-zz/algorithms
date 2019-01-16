cdb4a2769767019e132fb2b245c680cc18e0cbee
Peter Kim <peter-minho-kim@gmail.com>
2019-01-16 10:09:05 -0500

    Adds falsy bouncer

---
+++ FalsyBouncer.js
@@ -0,0 +1,19 @@
+// Remove all falsy values from an array.
+
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+function bouncer(arr) {
+  return arr.filter((item) => {
+    if (item) {
+      return item
+    }
+  })
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+// bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+// bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
+// bouncer([false, null, 0, NaN, undefined, ""]) should return [].
+// bouncer([1, null, NaN, 2, undefined]) should return [1, 2].
