fdbba47af7a76f7475c500290dad9c73f9829216
Peter Kim <peter-minho-kim@gmail.com>
2019-01-16 09:19:52 -0500

    Adds factorialize number

---
+++ FactorializeNumber.js
@@ -0,0 +1,32 @@
+// Return the factorial of the provided integer.
+
+// If the integer is represented with the letter n, a factorial is the 
+// product of all positive integers less than or equal to n.
+
+// Factorials are often represented with the shorthand notation n!
+
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+// Only integers greater than or equal to zero will be supplied to the function.
+
+function factorialize(num) {
+  if (num < 1) {
+    return 1
+  }
+
+  let answer = 1
+
+  for (let i = 1; i <= num; i++) {
+    answer *= i
+  }
+
+  return answer
+}
+
+factorialize(5);
+
+// factorialize(5) should return a number.
+// factorialize(5) should return 120.
+// factorialize(10) should return 3628800.
+// factorialize(20) should return 2432902008176640000.
+// factorialize(0) should return 1.
