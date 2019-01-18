b5bfbdf51072c032794a03cb3927de269a2eece2
Peter Kim <peter-minho-kim@gmail.com>
2019-01-17 22:51:54 -0500

    Adds sum all numbers in a range

---
+++ SumAllNumbers.js
@@ -0,0 +1,23 @@
+// We'll pass you an array of two numbers. 
+// Return the sum of those two numbers plus the sum of all the numbers between them.
+
+// The lowest number will not always come first.
+
+function sumAll(arr) {
+  let answer = 0
+
+  arr.sort((a, b) => a - b)
+  for (let i = arr[0]; i <= arr[1]; i++) {
+    answer += i
+  }
+
+  return answer
+}
+
+sumAll([1, 4]);
+
+// sumAll([1, 4]) should return a number.
+// sumAll([1, 4]) should return 10.
+// sumAll([4, 1]) should return 10.
+// sumAll([5, 10]) should return 45.
+// sumAll([10, 5]) should return 45.
